20197bc7df9f969e32885eba7a18e1a8b61ed411
patFish <fischer.pa7rick@gmail.com>
2020-02-20 11:09:46 +0100

    default

--- src/container/FormikField.js
+++ src/container/FormikField.js
@@ -1,12 +1,22 @@
 //FormikField.js
-import React from 'react';
+import React from 'react'
+import { Field, ErrorMessage } from 'formik'
+import TextField from '@material-ui/core/TextField'
 
-const FormikField = () => {
+const FormikField = ({ name, label, type = 'text', required = false }) => {
   return (
-    <div>
-      
-    </div>
-  );
-};
+    <React.Fragment>
+      <Field
+        required={required}
+        as={TextField}
+        fullWidth
+        label={label}
+        name={name}
+        type={type}
+        helperText={<ErrorMessage name={name} />}
+      />
+    </React.Fragment>
+  )
+}
 
-export default FormikField;
+export default FormikField
